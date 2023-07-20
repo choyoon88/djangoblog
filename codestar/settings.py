5ca0cf780837b757db18f42313e745a87bb8be9d
choyoon88 <azureluster@gmail.com>
2023-07-20 21:50:36 +0000

    added messages when login and logout / need to add message success when user leaves a comment

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-choyoon88-djangoblog-0rw0ljsd34i.ws-eu101.gitpod.io', 'myy-django-blog.herokuapp.com', 'localhost', 'myy-django-blog-b2e86372cb53.herokuapp.com']
+ALLOWED_HOSTS = ['8000-choyoon88-djangoblog-0rw0ljsd34i.ws-eu101.gitpod.io', 'myy-django-blog.herokuapp.com', 'localhost', 'myy-django-blog-b2e86372cb53.herokuapp.com', '8000-choyoon88-djangoblog-0rw0ljsd34i.ws-eu102.gitpod.io']
 
 
 # Application definition
@@ -56,6 +57,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
